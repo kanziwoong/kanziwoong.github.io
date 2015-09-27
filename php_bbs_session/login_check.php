9403066dd3a1f0b29234042970c14a81fdad70d5
kanziwoong <kanziwoong@gmail.com>
2015-09-28 03:04:19 +0900

    session login&out

---
+++ php_bbs_session/login_check.php
@@ -0,0 +1,22 @@
+<!--
+    echo "id: $_POST[id]";
+    echo "<br>";
+    echo "pw: $_POST[pw]";
+-->
+
+<?php
+    session_start();
+    $DB = mysql_connect( 'jusarang.kanziw.com', 'jusarang', 'a' );
+    $ret = mysql_select_db( 'jusarang', $DB );
+
+    $sql = "select * from user where id='$_POST[id]' and pw=password('$_POST[pw]')";
+    $result = mysql_query( $sql );
+
+    $count = mysql_num_rows( $result );
+    if ( $count > 0 ) {
+        header('location: http://jusarang.kanziw.com/login.php');
+    }
+    else {
+        echo "authentication fail... ";
+    }
+?>
