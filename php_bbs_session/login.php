01f6442047f1630c5eb4299a34e93366e2c347a4
kanziwoong <kanziwoong@gmail.com>
2015-09-28 03:57:04 +0900

    delete blank

--- php_bbs_session/login.php
+++ php_bbs_session/login.php
@@ -1,6 +1,6 @@
 <?php
-    session_start();
-    if(!isset($_SESSION['id'])) {
+session_start();
+    if(!$_SESSION['id']) {
 ?>
 <html>
     <head>
--- php_bbs_session/login_check.php
+++ php_bbs_session/login_check.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();
+session_start();
     $DB = mysql_connect( 'jusarang.kanziw.com', 'jusarang', 'a' );
     $ret = mysql_select_db( 'jusarang', $DB );
 
