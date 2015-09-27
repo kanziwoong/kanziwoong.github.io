5f66d9d3a9cfb4f0630e8316de1636ebd7d5a8cc
kanziwoong <kanziwoong@gmail.com>
2015-09-28 02:39:35 +0900

    db connect test

--- php_bbs_example/login_check.php
+++ php_bbs_example/login_check.php
@@ -1,21 +1,18 @@
 <?php
 
-    echo "id: $id";
-    echo "<br>";
-    echo "pw: $pw";
-
-    echo "<br>";
-    echo "<br>";
-
-    echo "id: $_GET[id]";
-    echo "<br>";
-    echo "pw: $_GET[pw]";
-
-    echo "<br>";
-    echo "<br>";
-
     echo "id: $_POST[id]";
     echo "<br>";
     echo "pw: $_POST[pw]";
+
+    $DB = mysql_connect( 'jusarang.kanziw.com', 'jusarang', 'a' );
+    $ret = mysql_select_db( 'test', $DB );
+
+    $sql = "select * from user";
+    $result = mysql_query( $sql );
+
+    while( $row = mysql_fetch_array( $result, MYSQL_NUM ) ) {
+      echo "num: $row[0] name: $row[1] id: $row[2] pw: $row[3]";
+      echo "<br>";
+    }
 
 ?>
