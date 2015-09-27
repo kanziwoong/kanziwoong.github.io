38d4a976a2c6c8428bb8d3fae07c4897ff7f40ff
kanziwoong <kanziwoong@gmail.com>
2015-09-28 02:29:59 +0900

    global test

--- php_bbs_example/login_check.php
+++ php_bbs_example/login_check.php
@@ -1,5 +1,21 @@
-<?php
+<?
+
     echo "id: $id";
     echo "<br>";
     echo "pw: $pw";
+
+    echo "<br>";
+    echo "<br>";
+
+    echo "id: $_GET[id]";
+    echo "<br>";
+    echo "pw: $_GET[pw]";
+
+    echo "<br>";
+    echo "<br>";
+
+    echo "id: $_POST[id]";
+    echo "<br>";
+    echo "pw: $_POST[pw]";
+
 ?>
