7f61d40a8b9e1e1aa97542797f4806c78abd04e2
jiwoongjung <kanziwoong@gmail.com>
2015-09-30 16:41:05 +0900

    include

--- jusarang/ptest.php
+++ jusarang/ptest.php
@@ -1,13 +1,13 @@
 <?php
-    error_reporting(E_ALL);
-    include_once('../simplehtmldom/simple_html_dom.php');
+    // error_reporting(E_ALL);
+    include('../simplehtmldom/simple_html_dom.php');
     $html = file_get_html('index.html');
     // Dumps the internal DOM tree back into string
     $str = $html;
 
     // Print it!
-    echo $str;
-    // echo $html;
+    // echo $str;
+    echo $html;
 ?>
 
 
