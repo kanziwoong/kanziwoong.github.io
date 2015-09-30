1304f6ad8c98e40d1f33b862470dc363cabe509d
jiwoongjung <kanziwoong@gmail.com>
2015-09-30 17:15:37 +0900

    count

--- php_bbs_cookie/login_check.php
+++ php_bbs_cookie/login_check.php
@@ -1,9 +1,7 @@
 <?php
-    echo "id: $_POST[id]";
-    echo "<br>";
-    echo "pw: $_POST[pw]";
-
-
+    // echo "id: $_POST[id]";
+    // echo "<br>";
+    // echo "pw: $_POST[pw]";
 
     $DB = mysql_connect( 'jusarang.kanziw.com', 'jusarang', 'a' );
     $ret = mysql_select_db( 'jusarang', $DB );
@@ -11,9 +9,8 @@
     $sql = "select * from user where id='$_POST[id]' and pw=password('$_POST[pw]')";
     $result = mysql_query( $sql );
 
-    echo $result;
-
-    // $count = mysql_num_rows( $result );
+    $count = mysql_num_rows( $result );
+    echo $count;
     // if ( $count > 0 ) {
     //     setcookie('cookie', $_POST[pw], 0, '/');
     //     header('location: ./login.php');
