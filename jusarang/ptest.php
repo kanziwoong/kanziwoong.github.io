0fc2670969df24ffb9e05024b630a1ed73593f4b
jiwoongjung <kanziwoong@gmail.com>
2015-09-30 16:55:10 +0900

    /etc/share/php  gogo

--- jusarang/ptest.php
+++ jusarang/ptest.php
@@ -1,9 +1,9 @@
 <?php
     // error_reporting(E_ALL);
-    // include '../simplehtmldom/simple_html_dom.php';
+    include '../simplehtmldom/simple_html_dom.php';
     $html = file_get_contents('index.html')
     // Dumps the internal DOM tree back into string
-    // $str = $html;
+    $str = $html;
 
     // Print it!
     // echo $str;
