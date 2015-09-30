33dbc8fe3f25576601b6b2036eade47e2bc95e60
jiwoongjung <kanziwoong@gmail.com>
2015-09-30 16:29:36 +0900

    own html gogo

--- jusarang/ptest.php
+++ jusarang/ptest.php
@@ -1,10 +1,11 @@
 <?php
-    $html = file_get_html('http://www.google.com/');
+    $html = file_get_html('http://aws.kanziw.com/jusarang/index.html');
     // Dumps the internal DOM tree back into string
     $str = $html;
 
     // Print it!
-    echo $str;
+    // echo $str;
+    echo $html;
 ?>
 
 
