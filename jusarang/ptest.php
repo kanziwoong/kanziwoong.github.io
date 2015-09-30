4060f8b0cd8da859537d6b4a754fb47216b11202
jiwoongjung <kanziwoong@gmail.com>
2015-09-30 16:36:06 +0900

    ptest.php now go?? can i go?

--- jusarang/ptest.php
+++ jusarang/ptest.php
@@ -1,11 +1,13 @@
 <?php
-    $html = file_get_html('http://aws.kanziw.com/jusarang/index.html');
+    error_reporting(E_ALL);
+    include_once('../simplehtmldom/simple_html_dom.php');
+    $html = file_get_html('index.html');
     // Dumps the internal DOM tree back into string
     $str = $html;
 
     // Print it!
     // echo $str;
-    echo $html;
+    echo $html
 ?>
 
 
